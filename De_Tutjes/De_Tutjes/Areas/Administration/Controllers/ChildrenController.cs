1c13ee2ee600ea639c218d8961ff946b704ab73a
Steven Janssens <steven.janssens@oplinter.be>
2017-02-28 22:44:59 +0100

    CreateChildModels (createchildandparentmodel + childrencontroller) some HTML & javascript additions

--- De_Tutjes/De_Tutjes/Areas/Administration/Controllers/ChildrenController.cs
+++ De_Tutjes/De_Tutjes/Areas/Administration/Controllers/ChildrenController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using De_Tutjes.Models;
+using De_Tutjes.Areas.Administration.Models;
 
 namespace De_Tutjes.Areas.Administration.Controllers
 {
@@ -30,9 +32,25 @@ namespace De_Tutjes.Areas.Administration.Controllers
         //POST: Administration/Children/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(int? step, )
+        public ActionResult Create(int? step, CreateChildAndParentsModel model)
         {
+            if (step == 1) {
+                // Child
+                Toddler toddler = new Toddler();
+                toddler.Person.FirstName = model.childFirstname;
+                toddler.Person.LastName = model.childLastname;
+                toddler.Person.Sex = model.childSex;
+                toddler.Person.BirthDate = model.childBirthdate;
+                toddler.Person.RegistrationDate = DateTime.Now;
 
+                // First Parent
+                Parent parent = new Parent();
+                parent.Person.FirstName = model.firstParentFirstname;
+                parent.Person.LastName = model.firstParentLastname;
+                parent.Person.BirthDate = model.firstParentBirthdate;
+                parent.Person.RegistrationDate = DateTime.Now;
+                
+            }
             return View();
         }
         
---
+++ De_Tutjes/De_Tutjes/Areas/Administration/Models/CreateChildModels.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace De_Tutjes.Areas.Administration.Models
+{
+    public class CreateChildAndParentsModel
+    {
+        // Child
+        public string childFirstname { get; set; }
+        public string childLastname { get; set; }
+        public string childSex { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime childBirthdate { get; set; }
+
+        // Parents
+        public bool isLivingTogether { get; set; }
+
+        // First Parent
+        public string firstParentFirstname { get; set; }
+        public string firstParentLastname { get; set; }
+        public DateTime firstParentBirthdate { get; set; }
+
+        public string firstParentStreet { get; set; }
+        public int firstParentHouseNumber { get; set; }
+        public string firstParentHouseBus { get; set; }
+        public int firstParentPostalCode { get; set; }
+        public string firstParentCity { get; set; }
+
+        public int firstParentPhoneHome { get; set; }
+        public int firstParentPhoneMobile { get; set; }
+        public int firstParentPhoneWork { get; set; }
+
+        [EmailAddress]
+        public string firstParentEmailAddress { get; set; }
+
+        // Second Parent
+        public string secondParentFirstname { get; set; }
+        public string secondParentLastname { get; set; }
+        public DateTime secondParentBirthdate { get; set; }
+
+        public string secondParentStreet { get; set; }
+        public int secondParentHouseNumber { get; set; }
+        public string secondParentHouseBus { get; set; }
+        public int secondParentPostalCode { get; set; }
+        public string secondParentCity { get; set; }
+
+        public int secondParentPhoneHome { get; set; }
+        public int secondParentPhoneMobile { get; set; }
+        public int secondParentPhoneWork { get; set; }
+
+        [EmailAddress]
+        public string secondParentEmailAddress { get; set; }
+
+    }
+
+    public class CreateReservationDaysModel
+    {
+
+    }
+
+    public class CreateMedicalModel
+    {
+
+    }
+
+    public class CreateFoodAndSleepingModel
+    {
+
+    }
+
+    public class CreateSpecialNoticesAndDayroutineModel
+    {
+
+    }
+}
